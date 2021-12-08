2bf6d2e3f24b3382bffdeb34468b2aa41db236d7
ohj4312 <ohj4312@naver.com>
2021-12-08 22:43:31 +0900

    language_coder / print (출력) -자가진단3 (21/12/08)

---
+++ JUNGOL_Solution/src/language_coder/print/Print3.java
@@ -0,0 +1,8 @@
+package language_coder.print;
+
+public class Print3 {
+	public static void main(String[] args) {
+		System.out.println("My name is Hong Gil Dong.");
+		System.out.println("I am 13 years old.");
+	}
+}
