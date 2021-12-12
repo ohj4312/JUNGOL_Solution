66816660731ece82b4e2bc1960a21ca7dd2344ed
ohj4312 <ohj4312@naver.com>
2021-12-12 17:18:06 +0900

    language_coder / print (출력) -자가진단3 (21/12/12) Sysoute.out.print() 활용

---
+++ JUNGOL_Solution/src/language_coder/print/Print4.java
@@ -0,0 +1,10 @@
+package language_coder.print;
+
+public class Print4 {
+	public static void main(String[] args) {
+		System.out.print(
+				"(@) (@)\r\n" + 
+				"(=^.^=)\r\n" + 
+				"(-m-m-)");
+	}
+}
