abe46bf30ab07d37325cad33d9d019f120af7c79
ohj4312 <ohj4312@naver.com>
2021-12-12 17:22:44 +0900

    language_coder / print (출력) -자가진단5 (21/12/12)

---
+++ JUNGOL_Solution/src/language_coder/print/Print5.java
@@ -0,0 +1,8 @@
+package language_coder.print;
+
+public class Print5 {
+	public static void main(String[] args) {
+		System.out.print("I can program well.\n");  
+		System.out.print("Dreams come true.");
+	}
+}
