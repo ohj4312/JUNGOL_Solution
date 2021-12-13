5e7bf24bb5d0e8fa563c43c0fd48e8ed7d59ebb8
ohj4312 <ohj4312@naver.com>
2021-12-14 00:16:26 +0900

    beginner_coder / 도형만들기1 - 1291:구구단 (21/12/13) 수정 : BufferedWriter

--- JUNGOL_Solution/src/Beginner_coder/makeFigure1/Gugudan1.java
+++ JUNGOL_Solution/src/Beginner_coder/makeFigure1/Gugudan1.java
@@ -1,14 +1,16 @@
 package Beginner_coder.makeFigure1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Gugudan1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 		/* Input Error 후 다시 출력하기 위해 코드 변경
 		 * String str=br.readLine(); //s와 e의 범위는 2~9이므로 charAt의 인덱스를 통해서 추출가능, 10이상일 경우
 		 * 불가능하다. int i=str.charAt(0)-'0'; int j=str.charAt(2)-'0';
@@ -19,7 +21,7 @@ public class Gugudan1 {
 		int j=Integer.parseInt(st.nextToken());
 		
 		while(i>9||j>9||i<2||j<2) {
-			System.out.println("INPUT ERROR!");
+			bw.write("INPUT ERROR!");
 			st=new StringTokenizer(br.readLine()," ");
 			i=Integer.parseInt(st.nextToken());
 			j=Integer.parseInt(st.nextToken());
@@ -41,6 +43,9 @@ public class Gugudan1 {
 			i=start;
 		}
 		
-		System.out.println(sb.toString());
+		bw.write(sb.toString());
+		
+		bw.flush();
+		bw.close();
 	}
 }
