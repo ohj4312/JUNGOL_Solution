b89a12a62c09e80bff2f4efcfec18c31e301372d
ohj4312 <ohj4312@naver.com>
2021-12-19 20:21:19 +0900

    beginner_coder / 도형만들기1 - 1339:문자삼각형2 (21/12/18) 19일 fix

--- JUNGOL_Solution/src/Beginner_coder/makeFigure1/CharTriangle2.java
+++ JUNGOL_Solution/src/Beginner_coder/makeFigure1/CharTriangle2.java
@@ -10,7 +10,7 @@ public class CharTriangle2 {
 		char c='A';
 		char[][] arr=new char[n][n];
 		
-		if(n<1||n>100) System.out.println("INPUT ERROR");
+		if(n<1||n>100||n%2==0) System.out.println("INPUT ERROR"); //홀수만 입력이여서 틀렸다.
 		else {
 			for(int j=n/2;j>=0;j--) {
 				for(int i=j;i<n-j;i++) {
@@ -21,10 +21,11 @@ public class CharTriangle2 {
 			}
 			
 			for(int i=0;i<n;i++) {
-				for(int j=0;j<n;j++) {
+				for(int j=0;j<=n/2;j++) {
 					if(arr[i][j]!=0) 
 						sb.append(arr[i][j]+" ");
 				}
+				sb.setLength(sb.length()-1);
 				sb.append("\n");
 			}
 			
